241ea82c4b5a20365101d32895d754e9c65c608b
Pikachye <bigcat13@rambler.ru>
2026-05-23 02:34:39 +0300

    fix api url

--- api/save.js
+++ api/save.js
@@ -20,7 +20,7 @@ export default async function handler(
     const response =
       await fetch(
 
-        'https://script.google.com/macros/s/AKfycbyCO0iwlKRUr6BcPf7TPw6-3WtcL1ayDLSGqPcAhuQo96O9cQEY_4ZOw3a4Uh48XOA/exec',
+      'https://script.google.com/macros/s/AKfycbyCO0iwlKRUr6BcPf7TPw6-3WtcL1ayDLSGqPcAhuQo96O9cQEY_4ZOw3a4Uh48XOA/exec',
 
         {
           method: 'POST',
