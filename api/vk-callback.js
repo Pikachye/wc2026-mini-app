ad14d90e1870149de48ea5be2d59b4888470b0ff
Pikachye <bigcat13@rambler.ru>
2026-06-02 21:17:09 +0300

    add vk callback2_1

--- api/vk-callback.js
+++ api/vk-callback.js
@@ -1,13 +1,3 @@
 export default function handler(req, res) {
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-
-  if (req.method !== 'POST') {
-    return res.status(200).send('ok');
-  }
-
-  if (req.body?.type === 'confirmation') {
-    return res.status(200).send('04159491');
-  }
-
-  return res.status(200).send('ok');
+  res.status(200).send('04159491');
 }
