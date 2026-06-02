d6780df2de7534901d828ddcacd87a112988b966
Pikachye <bigcat13@rambler.ru>
2026-06-02 21:19:16 +0300

    fix vk callback body parsing

--- api/vk-callback.js
+++ api/vk-callback.js
@@ -1,3 +1,21 @@
-export default function handler(req, res) {
-  res.status(200).send('04159491');
+export default async function handler(req, res) {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+
+  if (req.method !== 'POST') {
+    return res.status(200).send('ok');
+  }
+
+  const body = typeof req.body === 'string'
+    ? JSON.parse(req.body)
+    : req.body;
+
+  if (body.type === 'confirmation') {
+    return res.status(200).send('04159491');
+  }
+
+  if (body.type === 'message_new') {
+    // позже сюда добавим отправку ссылки
+  }
+
+  return res.status(200).send('ok');
 }
